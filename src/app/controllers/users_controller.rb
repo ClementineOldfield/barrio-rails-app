5a42d427e89663bb44caf7ab95fddaed0ae05c84
Juanma <juanmasanchezortiz@hotmail.com>
2019-04-30 11:27:33 +1000

    user´s controller, model and create, edit and show views

---
+++ src/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+    def create
+    #create new user
+    
+    end
+
+    def update 
+    #update user information
+    end
+
+    def edit 
+    #show the edit form
+    end
+
+    def destroy
+    #deletes user information
+    end
+
+    def show 
+    #show user profile
+    end
+    
+end
--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+    has_many :listings
+    has_one :address, dependent: :destroy 
+    has_many :purchases
+    has_one :image, as: :imageable, dependent: :destroy
 end
+
+
