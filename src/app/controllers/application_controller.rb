5e08e06760c906990fd90de46153a3c2f822c7d8
Juanma <juanmasanchezortiz@hotmail.com>
2019-04-30 16:42:41 +1000

    users controller methods and views

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  protected
+  # protect_from_forgery with: :exception
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :remember_me)}
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me)}
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :remember_me)}
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :remember_me)}
+  #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me)}
+  #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :remember_me)}
     
-  end
+  # end
   
 end
