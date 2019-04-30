2dc71bfa45109032dccefb914271c66ad861819c
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-04-30 11:24:52 +1000

    Added devise User Model.

---
+++ src/app/controllers/listings_controller.rb
@@ -0,0 +1,9 @@
+class ListingsController < ApplicationController
+  def index; end
+  def create; end
+  def show; end
+  def edit; end
+  def new; end
+  def update; end
+  def destroy; end
+end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  #devise_for :users, path: "/", path_names: {sign_in: "login"}
+  get "/", to: "pages#index", as: "root"
+
+  resources :listings
 end
