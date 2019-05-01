a9d276c2f3fe294fddfbd296b78ee9d4e7ea46e5
Juanma <juanmasanchezortiz@hotmail.com>
2019-05-01 12:36:49 +1000

    fixing bugs

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # def create
   # #create new user
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   # end
 
   # PUT /users/:id
-  def update 
+  # def update 
   # #update user information
   # @user = User.create(
   #   first_name: params.first_name,
@@ -25,20 +26,33 @@ class UsersController < ApplicationController
   #   encrypted_password: params.password,
   # )
   # redirect_to user_path
+  # end
+
+  def update
+    @user.bio = params[:bio]
+    @user.image.url = params[:image].url
+    redirect_to user_path
   end
 
   def edit 
   #show the edit form
-    @user = User.find(params[:id])
   end
 
   def destroy
   #deletes user information
+  User.find(params[:id]).destroy
+  redirect_to users_url
   end
 
 # GET /users/:id
-  def show 
+  def show
   #show user profile
+    
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 
