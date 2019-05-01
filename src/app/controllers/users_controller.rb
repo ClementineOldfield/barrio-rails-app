bcda0738382af6fb704ba0ebcde496934cbc99b7
Juanma <juanmasanchezortiz@hotmail.com>
2019-05-01 11:00:07 +1000

    edit user form

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -14,23 +14,32 @@ class UsersController < ApplicationController
   # redirect_to user_path
   # end
 
+  # PUT /users/:id
   def update 
-  #update user information
+  # #update user information
+  # @user = User.create(
+  #   first_name: params.first_name,
+  #   last_name: params.last_name,
+  #   bio: params.bio,
+  #   email: params.email,
+  #   encrypted_password: params.password,
+  # )
+  # redirect_to user_path
   end
 
   def edit 
   #show the edit form
+    @user = User.find(params[:id])
   end
 
   def destroy
   #deletes user information
   end
 
+# GET /users/:id
   def show 
   #show user profile
     @user = User.find(params[:id])
   end
 
-
-  
 end
