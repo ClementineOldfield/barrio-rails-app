8352b8434723526affe4667426db581e37cf194d
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-05-06 16:32:05 +1000

    Successfully integrated quantities to purchases.

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_044812) do
+ActiveRecord::Schema.define(version: 2019_05_06_052321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_05_03_044812) do
     t.string "stripe_session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
     t.index ["listing_id"], name: "index_carts_on_listing_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
