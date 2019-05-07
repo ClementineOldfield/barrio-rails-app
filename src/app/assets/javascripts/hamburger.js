0732fd5bd60fea4e50430a568fc35e272ece1d94
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-05-07 10:57:21 +1000

    Added styled hamburger menu.

---
+++ src/app/assets/javascripts/hamburger.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  let button = document.getElementById('hamburger');
+  button.addEventListener('click', function(e) {
+    e.currentTarget.classList.toggle('on');
+  });
+};
+
