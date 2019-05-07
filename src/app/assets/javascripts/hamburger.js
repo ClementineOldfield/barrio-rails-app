8ac4ffb864ff91e0267a073d5c50c8ad70f2b94f
Clementine  Oldfield <clementine.oldfield@gmail.com>
2019-05-07 16:43:34 +1000

    Fixed conditional in _listings partial for route checking.

--- src/app/assets/javascripts/hamburger.js
+++ src/app/assets/javascripts/hamburger.js
@@ -2,6 +2,7 @@ window.onload = function() {
   let button = document.getElementById('hamburger');
   button.addEventListener('click', function(e) {
     e.currentTarget.classList.toggle('on');
+    console.log(button);
   });
 };
 
